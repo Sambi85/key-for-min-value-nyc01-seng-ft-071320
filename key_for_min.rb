da3387c331652a93413e1f6c783695c8448cb28a
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-21 23:32:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 var = 
 name_hash.each do |name, number|
-  number.collect do |number| var << number
+  number.collect do |number| var << number.to_a
     var
     end
   end
