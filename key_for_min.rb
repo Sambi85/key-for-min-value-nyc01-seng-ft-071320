fd2267333212191cb94499a3ae710680081625f1
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-21 23:29:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+var = 
+name_hash.each do |key, value|
+  value.collect |value| var << value
+    if value == var
+     return key
+      end
+    end
+  end
 end
